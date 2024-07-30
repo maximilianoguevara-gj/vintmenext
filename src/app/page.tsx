36705dc3c533c672c498018a0b698058560bf60f
maximilianoguevara-gf <maximiliano.guevara@gojiraf.com>
2024-07-30 16:48:51 -0300

    404

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-// pages/404.js
-import Link from 'next/link';
-
-const Custom404 = () => {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.errorCode}>404</h1>
-      <p className={styles.errorMessage}>Lo sentimos, la página que estás buscando no existe.</p>
-      <Link href="/">
-        <a className={styles.homeLink}>Volver a la página principal</a>
-      </Link>
-    </div>
-  );
-};
-
-export default Custom404;
-
+export default function Home() {
+  return <main>404</main>;
+}
