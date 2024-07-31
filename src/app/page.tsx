e9b7583bfcfb05d0ae826002da90b80aa0d171ce
Vintme <116039175+Vintme@users.noreply.github.com>
2024-07-31 17:10:37 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <main>404</main>;
-}
+import React from "react";
+
+export const Image = (): JSX.Element => {
+  return (
+    <div className="w-[1440px] h-[600px]">
+      <img
+        className="fixed w-[1440px] h-[600px] top-0 left-0 object-cover"
+        alt="Fondo vintme figma"
+        src="fondo-vintme-figma-1.png"
+      />
+    </div>
+  );
+};
