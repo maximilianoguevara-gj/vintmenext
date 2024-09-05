e839ca40857944d25659e07a9707e6e68b4171ca
Vintme <vintmeok@gmail.com>
2024-09-05 07:41:41 -0300

    🚧 In progress

---
+++ vintme2/src/app/page.tsx
@@ -0,0 +1,14 @@
+// app/page.tsx
+
+import React from 'react';
+
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-2xl font-bold text-gray-800 animate-bounce">
+        Launching...
+      </div>
+    </main>
+  );
+}
